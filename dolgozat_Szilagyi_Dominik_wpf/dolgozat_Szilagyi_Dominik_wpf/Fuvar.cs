87fc98c69f23f24e41e3b052c4273d8b36db0e2e
Sushi <70635877+Sushkoo@users.noreply.github.com>
2023-09-19 13:07:01 +0200

    Add files via upload

---
+++ dolgozat_Szilagyi_Dominik_wpf/dolgozat_Szilagyi_Dominik_wpf/Fuvar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolgozat_Szilagyi_Dominik_wpf
+{
+     public class Fuvar
+    {
+        int taxi_id;
+        string indulas;
+        int idotartam;
+        double tavolsag;
+        double viteldij;
+        double borravalo;
+        string fizetes_modja;
+
+
+        public Fuvar(int taxi_id, string indulas, int idotartam, double tavolsag, double viteldij, double borravalo, string fizetes_modja)
+        {
+            this.taxi_id = taxi_id;
+            this.indulas = indulas;
+            this.idotartam = idotartam;
+            this.tavolsag = tavolsag;
+            this.viteldij = viteldij;
+            this.borravalo = borravalo;
+            this.fizetes_modja = fizetes_modja;
+        }
+
+        public int Taxi_id { get => taxi_id; }
+        public string Indulas { get => indulas; }
+        public int Idotartam { get => idotartam; }
+        public double Tavolsag { get => tavolsag; }
+        public double Viteldij { get => viteldij; }
+        public double Borravalo { get => borravalo; }
+        public string Fizetes_modja { get => fizetes_modja; }
+    }
+}
---
+++ dolgozat_Szilagyi_Dominik_wpf/dolgozat_Szilagyi_Dominik_wpf/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dolgozat_Szilagyi_Dominik_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Fuvar> fuvarok = new List<Fuvar>();
+
+        private void Button_Click(object sender, RoutedEventArgs e) //beolvasas
+        {
+     
+            using (StreamReader sr = new("fuvar.csv"))
+            {
+                sr.ReadLine().Skip(1);
+                    while (!sr.EndOfStream)
+                    {
+                        string[] sor = sr.ReadLine().Split(";");
+                        fuvarok.Add(new Fuvar(
+                            int.Parse(sor[0]),
+                            sor[1],
+                            int.Parse(sor[2]),
+                            double.Parse(sor[3]),
+                            double.Parse(sor[4]),
+                            double.Parse(sor[5]),
+                            sor[6]
+
+                   ));
+                    }
+                }
+           
+            MessageBox.Show("Sikerult");
+
+            
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) //3. feladat
+        {
+            
+            MessageBox.Show($"{fuvarok.Count()} fuvar");
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e) //4. feladat
+        {
+
+            double bevetel = 0;
+            int hanyszor = 0;
+
+            foreach (var fuvar in fuvarok)
+            {
+                while (fuvar.Taxi_id==6185)
+                {
+                    bevetel += fuvar.Viteldij + fuvar.Borravalo;
+                    hanyszor++;
+                }
+            }
+
+            MessageBox.Show($"{hanyszor} fuvar alatt: {bevetel}$");
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) // 5.feladat
+        {
+            int bankkartyaval = 0;
+            int keszpenz = 0;
+            int vitatott = 0;
+            int ingyenes = 0;
+            int ismeretlen = 0;
+
+            foreach (var fuvar in fuvarok)
+            {
+                switch (fuvar.Fizetes_modja)
+                {
+                    case ("bankkártya"):
+                        bankkartyaval++;
+                        break;
+                    case ("készpénz"):
+                        keszpenz++;
+                        break;
+                    case ("vitatott"):
+                        vitatott++;
+                        break;
+                    case ("ingyenes"):
+                        ingyenes++;
+                        break;
+                    case ("ismeretlen"):
+                        ismeretlen++;
+                        break;
+                }
+            }        
+
+            MessageBox.Show($"bankkártya: {bankkartyaval} készpénz: {keszpenz} vitatott: {vitatott} ingyenes: {ingyenes} ismeretlen {ismeretlen}");
+
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e) //8. feladat
+        {
+            foreach (var fuvar in fuvarok)
+            {
+                if (fuvar.Idotartam>0 && fuvar.Viteldij>0 && fuvar.Tavolsag==0)
+                {
+                    string writeText = fuvar.Taxi_id+fuvar.Indulas+fuvar.Idotartam+fuvar.Viteldij+fuvar.Borravalo+fuvar.Fizetes_modja ;
+                    File.WriteAllText("hibak.txt", writeText, Encoding.UTF8);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
